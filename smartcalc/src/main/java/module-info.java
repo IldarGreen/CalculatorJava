ba5063db3fac1a1120e93684504f0bdc4a117fd4
Vanesa Bowman <vanesabo@ob-e8.nsk.21-school.ru>
2024-04-21 13:33:35 +0700

    new start

---
+++ smartcalc/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.greenone.smartcalc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.greenone.smartcalc to javafx.fxml;
+    exports com.greenone.smartcalc;
+}
